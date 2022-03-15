4ff7183d41b5972eb0436f14af8d8f79f949d996
COTHSC <thierryscully.colomban@gmail.com>
2022-03-15 13:03:52 +0100

    Puttin' on my sockette's

--- core/core_experiment.c
+++ core/core_experiment.c
@@ -7,7 +7,6 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <fcntl.h>
-//#include <signal.h>
 #define MAX_EVENTS 10
 #define READ_SIZE 30000
 #define SERVER_PORT 18001
@@ -63,24 +62,17 @@ void make_fd_non_blocking(int fd){
   check((fcntl(fd, F_SETFL, flags)), "fcntl error");
 }
 
-//void handle_sig(int sig, siginfo_t *info, void *ucontext){
-//  (void)(info);
-//  (void)(ucontext);
-//  printf("SIG HANDLER...\n");
-//  if (sig == SIGINT){
-//    printf("Handling SIGINT...\n");
-//    close(server_fd);
-//    check(-1, "server error");
-//  }
-//}
-//
-//void set_signal_handling(void){
-//  static struct sigaction sa;
-//  sa.sa_sigaction = &handle_sig;
-//	sa.sa_flags = SA_RESTART | SA_SIGINFO;
-//    //Set up a signal listener
-//  check(sigaction(SIGINT, &sa, NULL), "sigaction error");
-//}
+void check_error_flags(int event)
+{
+  if (event & EPOLLHUP || event & EPOLLERR)
+  {
+    if (event & EPOLLHUP)
+      write(0, "EPPOLHUP\n", 9);
+    if (event & EPOLLERR)
+      write(0, "EPPOLERR\n", 9);
+    exit(1);
+  }
+}
 
 int main(){
 
@@ -89,31 +81,24 @@ int main(){
   int epoll_fd;
   uint8_t sent_line[] = "HTTP/1.1 200 OK \n\n <html><head>SO BAAAAAD</head></html>\n";
   uint8_t received_line[READ_SIZE + 1];
-  // int epollin_value = EPOLLIN;
-  // int epollout_value = EPOLLOUT;
-  // int epollhup_value = EPOLLHUP;
   int read_bytes;
   int count_of_fd_actualized = 0;
   server_fd = setup_server(SERVER_PORT, MAX_QUEUE);
 
   //Prep a set of epoll event struct to register listened events
-  struct epoll_event *events_received = calloc(MAX_EVENTS, sizeof(struct epoll_event));
+  struct epoll_event *events = calloc(MAX_EVENTS, sizeof(struct epoll_event));
 
   //Set up an epoll instance
   check((epoll_fd = epoll_create(1)), "epoll error");
   //Use epoll_ctl to add the server socket to epoll to monitor events from the server
   monitor_socket_action(epoll_fd, server_fd, EPOLLIN | EPOLLOUT, EPOLL_CTL_ADD);
+
   while (1){
-    //count_of_fd_actualized = 0;
-    printf("waiting for a connection on port %d\n", SERVER_PORT);
-    printf("count of fd actualized %d\n", count_of_fd_actualized);
-    check((count_of_fd_actualized = epoll_wait(epoll_fd, events_received, MAX_EVENTS, -1)), "epoll_wait error");
+    check((count_of_fd_actualized = epoll_wait(epoll_fd, events, MAX_EVENTS, -1)), "epoll_wait error");
     for(int i = 0; i < count_of_fd_actualized; i++){
-      printf("idx: %d - fd: %d - event mask %d\n", i, events_received[i].data.fd, events_received[i].events);
-      if (events_received[i].data.fd == server_fd){
-        if (events_received[i].events & EPOLLOUT) write(0, "EPPOLOUT SERV\n", 14);
-        if (events_received[i].events & EPOLLIN)  write(0, "EPPOLINN SERV\n", 14);
-        if (events_received[i].events & EPOLLHUP || events_received[i].events & EPOLLERR){
+      
+      if (events[i].data.fd == server_fd){
+        if (events[i].events & EPOLLHUP || events[i].events & EPOLLERR){
           close(server_fd);
           check(-1, "server error");
         }
@@ -123,30 +108,21 @@ int main(){
         printf("Connexion accepted for fd: %d\n", connexion_fd);
       }
       else {
-        if (events_received[i].events & EPOLLHUP || events_received[i].events & EPOLLERR){
-          if (events_received[i].events & EPOLLHUP) write(0, "EPPOLHUP\n", 9);
-          if (events_received[i].events & EPOLLERR) write(0, "EPPOLERR\n", 9);
-          close(events_received[i].data.fd);
-          break;
-        }
-        if (events_received[i].events & EPOLLOUT) write(0, "EPPOLOUT\n", 9);
-        if (events_received[i].events & EPOLLIN) write(0, "EPPOLINN\n", 9);
+        check_error_flags(events[i].events);
         bzero(&received_line, READ_SIZE);
-        while ((read_bytes = read(events_received[i].data.fd, received_line, READ_SIZE)) > 0){
+        while ((read_bytes = read(events[i].data.fd, received_line, READ_SIZE)) > 0){
           write(STDOUT_FILENO, received_line, read_bytes); 
           if (received_line[read_bytes - 1] == '\n'){
             break;
           }
-          bzero(received_line, READ_SIZE);
         }
         check(read_bytes, "read error");
         if (read_bytes == 0){
-          printf("Closing connexion for fd: %d\n", events_received[i].data.fd);
-          close(events_received[i].data.fd);
+          printf("Closing connexion for fd: %d\n", events[i].data.fd);
+          close(events[i].data.fd);
           break;
         }
         write(connexion_fd, (char*)sent_line, strlen((char *)sent_line));
-        //write(connexion_fd, "\0", 1);
       }
     }
   }
