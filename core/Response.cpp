4738cc9ac15ad3fe544a1b04504dd803502379a3
COTHSC <thierryscully.colomban@gmail.com>
2022-03-30 17:42:08 +0200

    response handling multiple error codes and look up for error pages

--- core/Response.cpp
+++ core/Response.cpp
@@ -7,6 +7,47 @@
 #include<sys/socket.h>
 #include <map>
 
+std::string getErrorContent(int errCode)
+{
+    std::map<int, std::string> ErrCodeMap =
+        {{400, "Bad Request"},
+         {401, "Unauthorized"},
+         {402, "Payment Required"},
+         {403, "Forbiden"},
+         {404, "hey you, out there in the dark feeling lonely feeling old."},
+         {405, "Method Not Allowed"},
+         {406, "Not Acceptable"},
+         {407, "Proxy Authentication Required"},
+         {408, "Request Timeout"},
+         {409, "Conflict"},
+         {410, "Gone"},
+         {411, "Length Required"},
+         {412, "Precondition Failed"},
+         {413, "Payload Too Large"},
+         {414, "URI Too Long"},
+         {415, "Unsupported Media Type"},
+         {416, "Range Not Satisfiable"},
+         {417, "Expectation Failed"},
+         {418, "I'm a teapot"},
+         {421, "Misdirected Request"},
+         {422, "Unprocessable Entity"},
+         {423, "Locked"}};
+
+
+
+    char buf[3];
+    sprintf(buf, "%d", errCode);
+    std::string errPathname = "." + std::string(ROOT_DIR) + "/" + buf + ".html"; 
+    std::cout << errPathname << std::endl;
+    if(doesFileExist(errPathname))
+        return (readFileIntoString(errPathname));
+    std::map<int, std::string>::iterator it = ErrCodeMap.find(errCode);
+    if (it != ErrCodeMap.end())
+        return it->second;
+    return "";
+}
+
+
 std::string Response::codeToReasonPhrase(int statusCode){
     // ! To be modified with a table of code
     (void)statusCode;
@@ -72,23 +113,18 @@ std::string Response::timeAsString()
 
 void Response::addBody(std::string pathname)
 {
-    std::ifstream input_file(pathname);
+    /*
+    if response code > 200
+    add body uses the content from the map in thhe response.
+    */
     char buf[10];
-    if (!doesFileExist(pathname))
-    {
-        input_file.close();
-        std::cerr << "Could not open the file - '"
-                  << pathname << "'" << std::endl;
-        //std::string errorPageNotFound = ;
-        std::string errorPathname = "." + std::string(ROOT_DIR) + std::string("/404.html"); 
-        input_file.open(errorPathname, std::ifstream::in);
-        if(!doesFileExist(errorPathname)){
-            std::cout << "TU FAIS DE LA MERDE\n";
-        }
-        //exit(EXIT_FAILURE);
-    }
-    _Content = std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
-    // std::cout << _Content << std::endl;
+
+    if (_statusCode >= 300)
+        _Content = getErrorContent(_statusCode);    
+    else{
+        std::ifstream input_file(pathname.c_str());
+        _Content = std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+   }
     sprintf(buf, "%lu", _Content.size());
     _ContentLength = std::string(buf);
 }
