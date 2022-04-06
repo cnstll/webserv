4c0e724b07b80b54248a10f05d53d9adb8649c21
cnstll <calle@student.42.fr>
2022-04-06 19:18:47 +0200

    exception handling in update script | updating html pages with buttons

--- core/server_root/upload_resource.py
+++ core/server_root/upload_resource.py
@@ -14,9 +14,21 @@ error_body = " <!DOCTYPE html>\
 <html>\
 <body>\
 \
-<h1>Error :( </h1>\
+<h1>Error 400 :( </h1>\
 <p> Did you send a file ?</p>\
-  <button onclick=\"window.location.href='http://localhost:18000/upload_files.html';\">\
+  <button onclick=\"window.location.href='http://localhost:18000/upload_resource.html';\">\
+  Send again\
+\
+</body>\
+</html> "
+
+exception_body = " <!DOCTYPE html>\
+<html>\
+<body>\
+\
+<h1>Error 500 :( </h1>\
+<p> My bad my bad... </p>\
+  <button onclick=\"window.location.href='http://localhost:18000/upload_resource.html';\">\
   Send again\
 \
 </body>\
@@ -40,29 +52,40 @@ valid_body = " <!DOCTYPE html>\
 # Listing server files
 root = "/core/server_root"
 # Accessing form data
-#eprint("ENV: " + str(os.environ))
-form = cgi.FieldStorage()
-# Retrieving filename
-fileitem = form["filename"]
-if fileitem.filename:
-  # Checking for leading path and striping it
-  fn = os.path.basename(fileitem.filename.replace("\\", "/" ))
-  # Copying uploaded files
-  open("." + root + '/tmp/' + fn, 'wb').write(fileitem.file.read())
-  # Response Header and Body sent to stdout
-  print("HTTP/1.1 200 OK")
-  currentDate.printFormatedCurrentDate()
-  print("Connection: Keep-Alive")
-  print("Content-Type: text/html")
-  print("Content-Length: " + str(len(valid_body)))
-  print("\n")
-  print(valid_body)
-else:
+try:
+  form = cgi.FieldStorage()
+  # eprint("INFO: " + str(form))
+  # Retrieving filename
+  fileitem = form["filename"]
+except:
   # if no file has been sent, error msg
-  print("HTTP/1.1 400 Bad Request")
+  print("HTTP/1.1 500 Internal Server Error")
   currentDate.printFormatedCurrentDate()
   print("Connection: Keep-Alive")
   print("Content-Type: text/html")
-  print("Content-Length: " + str(len(error_body)))
+  print("Content-Length: " + str(len(exception_body)))
   print("\n")
-  print(error_body)
+  print(exception_body)
+else:
+  if fileitem.filename:
+    # Checking for leading path and striping it
+    fn = os.path.basename(fileitem.filename.replace("\\", "/" ))
+    # Copying uploaded files
+    open("." + root + '/tmp/' + fn, 'wb').write(fileitem.file.read())
+    # Response Header and Body sent to stdout
+    print("HTTP/1.1 200 OK")
+    currentDate.printFormatedCurrentDate()
+    print("Connection: Keep-Alive")
+    print("Content-Type: text/html")
+    print("Content-Length: " + str(len(valid_body)))
+    print("\n")
+    print(valid_body)
+  else:
+      # if no file has been sent, error msg
+    print("HTTP/1.1 400 Bad Request")
+    currentDate.printFormatedCurrentDate()
+    print("Connection: Keep-Alive")
+    print("Content-Type: text/html")
+    print("Content-Length: " + str(len(error_body)))
+    print("\n")
+    print(error_body)
