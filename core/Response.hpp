eecb85df91dbb4509aa5cc83c0e833c7a1421c92
COTHSC <thierryscully.colomban@gmail.com>
2022-04-11 17:13:33 +0200

    Some cleanup on correct status codes

--- core/Response.hpp
+++ core/Response.hpp
@@ -31,8 +31,56 @@ private:
     std::string _Location = "/index.html";
     std::string timeAsString();
     const char *convertToCString();
+    std::map<int, std::string> ErrCodeMap =
+        {{200, "OK"},
+         {201, "Created"},
+         {300, "Multiple Choice"},
+         {301, "Moved Permanently"},
+         {302, "Found"},
+         {303, "See Other"},
+         {304, "Not Modified"},
+         {307, "Temporary Redirect"},
+         {308, "Permanent Redirect"},
+         {400, "Bad Request"},
+         {401, "Unauthorized"},
+         {402, "Payment Required"},
+         {403, "Forbiden"},
+         {404, "Not Found"},
+         {405, "Method Not Allowed"},
+         {406, "Not Acceptable"},
+         {407, "Proxy Authentication Required"},
+         {408, "Request Timeout"},
+         {409, "Conflict"},
+         {410, "Gone"},
+         {411, "Length Required"},
+         {412, "Precondition Failed"},
+         {413, "Payload Too Large"},
+         {414, "URI Too Long"},
+         {415, "Unsupported Media Type"},
+         {416, "Range Not Satisfiable"},
+         {417, "Expectation Failed"},
+         {418, "I'm a teapot"},
+         {421, "Misdirected Request"},
+         {422, "Unprocessable Entity"},
+         {423, "Locked"},
+         {426, "Upgrade Required"},
+         {428, "Precondition Required"},
+         {429, "Too Many Requests"},
+         {431, "Request Header Fields Too Large"},
+         {451, "Unavailable For Legal Reasons"},
+         {500, "Internal Server Error"},
+         {501, "Not Implemented"},
+         {502, "Bad Gateway"},
+         {503, "Service Unavailable"},
+         {504, "Gateway Timeout"},
+         {505, "HTTP Version Not Supported"},
+         {506, "Variant Also Negotiates"},
+         {510, "Not Extended"},
+         {511, "Network Authentication Required"}};
 
 public:
+    std::string getErrorContent(int errCode);
+    std::string getCodeStatus(int errCode);
     Response(int code = 500);
     std::string codeToReasonPhrase(int);
     Response(const Response &newResponse);
