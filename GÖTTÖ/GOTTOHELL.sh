c47ae6c411c00153ea30597814a959621a2716ed
COTHSC <thierryscully.colomban@gmail.com>
2022-04-22 16:51:39 +0000

    Better launch script for GOTTO

--- GÖTTÖ/GOTTOHELL.sh
+++ GÖTTÖ/GOTTOHELL.sh
@@ -1,5 +1,9 @@
 
 echo "MAKE SURE YOUR SERVER IS RUNNING, AND RUNNING FAST"
 
+go get github.com/charmbracelet/bubbletea
+go get github.com/muesli/termenv
+go get github.com/charmbracelet/bubbles/textinput
+
 go build GOTTO.go
 ./GOTTO
